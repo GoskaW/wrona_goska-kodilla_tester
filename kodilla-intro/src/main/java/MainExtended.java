2461084f512308d548cdb2c7b5fdca49c66d4b5b
Malgorzata Wrona <wrona.goska@gmail.com>
2022-09-18 19:26:57 +0200

    MainExtended

---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,16 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String myName = "Malgorzata";
+        int age = 32;
+        double height = 1.69;
+        char lastSurnameLetter = 'W';
+        boolean isHavingFunWithJava = true;
+        char driverLicense = 'B';
+        System.out.println(myName);
+        System.out.println(age);
+        System.out.println(height);
+        System.out.println(lastSurnameLetter);
+        System.out.println(isHavingFunWithJava);
+        System.out.println(driverLicense);
+            }
+}

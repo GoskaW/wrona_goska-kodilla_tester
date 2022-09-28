ad94678cd78a0446f34a0fcb28d3cc7e92a3c7d4
Malgorzata Wrona <wrona.goska@gmail.com>
2022-09-28 14:53:07 +0200

    SimpleArray

---
+++ kodilla-intro/src/main/java/SimpleArray.java
@@ -0,0 +1,15 @@
+public class SimpleArray {
+    public static void main(String[] args) {
+        String[] names= new String [5];
+        names[0] = "Jerry";
+                names[1] = "John";
+        names[2] = "Clark";
+        names[3] = "Edward";
+        names[4] = "Jonathan";
+               System.out.println(names[3]);
+        int numberOfElements = names.length;
+        System.out.println(names.length);
+        System.out.println("Moja tablica zawiera 5 elementów");
+
+    }
+}

f559a663e11513518b5a8c3300aeec993cba68c6
Malgorzata Wrona <wrona.goska@gmail.com>
2022-09-28 11:46:56 +0200

    wrzesien2022

--- kodilla-intro/src/main/java/FirstClass.java
+++ kodilla-intro/src/main/java/FirstClass.java
@@ -1,5 +1,16 @@
 public class FirstClass {
     public static void main(String[] args) {
         System.out.println("Hello from FirstClass!");
-    }
+
+        if (1 > 2){
+            System.out.println("Amazing! One is bigger than two!");
+        }
+        if (1 > 2) {
+            System.out.println("Amazing! One is bigger than Two)");
+        }
+        else {
+            System.out.println("Nothing change, one is still smaller than two");
+
+        }
+            }
 }

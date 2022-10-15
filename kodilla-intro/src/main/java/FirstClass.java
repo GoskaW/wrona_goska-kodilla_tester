d6529e0aea858dd382a8632db1b1c582ba19c451
Malgorzata Wrona <wrona.goska@gmail.com>
2022-10-15 14:59:40 +0200

    NewFirstClass

--- kodilla-intro/src/main/java/FirstClass.java
+++ kodilla-intro/src/main/java/FirstClass.java
@@ -1,16 +1,16 @@
 public class FirstClass {
     public static void main(String[] args) {
-        System.out.println("Hello from FirstClass!");
+        Notebook notebook = new Notebook(600, 1000);
+        System.out.println(notebook.weight + " " + notebook.price);
+        notebook.checkPrice();
 
-        if (1 > 2){
-            System.out.println("Amazing! One is bigger than two!");
-        }
-        if (1 > 2) {
-            System.out.println("Amazing! One is bigger than Two)");
-        }
-        else {
-            System.out.println("Nothing change, one is still smaller than two");
+        Notebook heavyNotebook = new Notebook(2000, 1500);
+        System.out.println(heavyNotebook.weight + " " + heavyNotebook.price);
+        heavyNotebook.checkPrice();
 
-        }
-            }
+        Notebook oldNotebook = new Notebook(1600, 500);
+        System.out.println(oldNotebook.weight + " " + oldNotebook.price);
+        oldNotebook.checkPrice();
+
+    }
 }
---
+++ kodilla-intro/src/main/java/Notebook.java
@@ -0,0 +1,22 @@
+public class Notebook {
+    int weight;
+    int price;
+
+    public Notebook(int weight, int price) {
+        this.weight = weight;
+        this.price = price;
+    }
+
+    public void checkPrice() {
+        if (this.price < 600) {
+            System.out.println("This notebook is very cheap.");
+        } else if (this.price > 600 && this.price < 1000) {
+                System.out.println("The price is good");
+        } else {
+            System.out.println("This notebook is expensive.");
+        }
+    }
+}
+
+
+

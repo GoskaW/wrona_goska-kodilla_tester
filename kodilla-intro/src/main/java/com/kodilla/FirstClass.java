ebbac789b42e27e1d5caedb08baa36bd738472f1
Malgorzata Wrona <wrona.goska@gmail.com>
2022-10-23 15:36:02 +0200

    Notebook

--- kodilla-intro/src/main/java/com/kodilla/FirstClass.java
+++ kodilla-intro/src/main/java/com/kodilla/FirstClass.java
@@ -5,6 +5,7 @@ public class FirstClass {
             Notebook notebook = new Notebook(600, 1000, 2010);
             System.out.println(notebook.weight + notebook.price); // to sumuje dwa parametry
             System.out.println(notebook.weight + " " + notebook.price); // to wyrzuca parametry osobno
+            System.out.println("notebook.year: " + notebook.year);
             notebook.checkPrice();
             notebook.checkWeight();
             notebook.checkYear();
@@ -13,6 +14,7 @@ public class FirstClass {
             Notebook heavyNotebook = new Notebook(2000, 1500, 2000);
             System.out.println("heavyNotebook.weight: " + heavyNotebook.weight);
             System.out.println("heavyNotebook.price: " + heavyNotebook.price);
+            System.out.println("heavyNotebook.year: " + heavyNotebook.year);
             heavyNotebook.checkPrice();
             heavyNotebook.checkWeight();
             heavyNotebook.checkYear();
@@ -20,6 +22,7 @@ public class FirstClass {
             Notebook oldNotebook = new Notebook(1600, 500, 2005);
             System.out.println("oldNotebook.weight: " + oldNotebook.weight);
             System.out.println("oldNotebook.price: " + oldNotebook.price);
+            System.out.println("oldNotebook.year: " + oldNotebook.year);
             oldNotebook.checkPrice();
             oldNotebook.checkWeight();
             oldNotebook.checkYear();

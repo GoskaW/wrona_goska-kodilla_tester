e07c3e9dbc4124c0dda8b52ab9c44dea50fb8730
Malgorzata Wrona <wrona.goska@gmail.com>
2022-10-24 19:24:39 +0200

    Book

---
+++ kodilla-intro/src/main/java/com/kodilla/Book.java
@@ -0,0 +1,23 @@
+package com.kodilla;
+
+public class Book {
+    private String author;
+    private String title;
+
+    String author ("Janina Bąk") {
+    return author;
+    }
+    String title("Statystycznie rzecz biorąc"){
+        return title;
+    }
+    public static void main(String[] args) {
+        Book book = new Book (); // nie bardzo wiem jak wywołać metodę of, bo gdy tworzę ją w podobny sposób jak Scanner, pojawia się bład
+
+
+    }
+
+
+}
+
+
+

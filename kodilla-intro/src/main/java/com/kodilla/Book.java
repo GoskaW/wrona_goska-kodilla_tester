9624c9219acb1caef9cce2034b17c07236dea937
Malgorzata Wrona <wrona.goska@gmail.com>
2022-10-28 22:33:43 +0200

    book

--- kodilla-intro/src/main/java/com/kodilla/Book.java
+++ kodilla-intro/src/main/java/com/kodilla/Book.java
@@ -1,22 +1,26 @@
 package com.kodilla;
 
 public class Book {
-        private String author;
-        private String title;
+    private String author;
+    private String title;
 
-        public Book(String author, String title){
-                this.author = author;
-                this.title = title;
-        }
+    public Book(String author, String title) {
+        this.author = author;
+        this.title = title;
+    }
 
-         static Book of ("Janina Bąk", "Statystycznie rzecz biorąc.") {
-          Book book = new Book();
-          Book book = Book.of();
-        }
 
-        }
+    static Book of() {
+        Book book = new Book("Janina Bąk", "Statystycznie rzecz biorąc.");
+        return;
+    }
+                // poprawiłam obiekt, ale nie wiem jak to wyciągnąć. Return nie zwraca, bo brakuje mu wartości. próbowałam getterami. Poddaje sie, pokaż mi proszę jak to powinno wyglądać.
 
-                                // statyczna metoda of przyjmuje dwa argumenty typu string. Jest stworzony obiekt oraz wywołana metoda
+
+}
+
+
+                                //
 
 
 

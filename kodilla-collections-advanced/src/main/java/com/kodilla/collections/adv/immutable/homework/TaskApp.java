3a9f0668cad75c0730efab17bb474b1298a8ebc6
Malgorzata Wrona <wrona.goska@gmail.com>
2022-11-05 18:40:18 +0100

    Task

---
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/homework/TaskApp.java
@@ -0,0 +1,8 @@
+package com.kodilla.collections.adv.immutable.homework;
+
+public class TaskApp {
+    public static void main(String[] args) {
+        Task task = new Task ("Harry Potter", 2);
+        TaskRecord taskRecord = new TaskRecord ("Die Hard", 3, 1998);
+    }
+}

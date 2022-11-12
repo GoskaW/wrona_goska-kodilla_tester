08f5e59863e798af8146d556e36dbbb71415c71c
Malgorzata Wrona <wrona.goska@gmail.com>
2022-11-12 12:27:19 +0100

    Application

---
+++ kodilla-stream/src/test/java/com/kodilla/optional/homework/ApplicationTestSuite.java
@@ -0,0 +1,16 @@
+package com.kodilla.optional.homework;
+
+import org.junit.jupiter.api.Test;
+
+public class ApplicationTestSuite {
+
+    @Test
+    public void shouldShowCorrectTeacherName(){
+
+    }
+
+    @Test
+    public void shouldReturnWhenNull(){
+
+    }
+}

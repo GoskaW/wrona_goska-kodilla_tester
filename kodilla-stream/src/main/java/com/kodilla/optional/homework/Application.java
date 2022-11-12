a2b630cd305bc99b01f6959b3eef1c65cbf1415d
Malgorzata Wrona <wrona.goska@gmail.com>
2022-11-12 12:13:39 +0100

    Application

--- kodilla-stream/src/main/java/com/kodilla/optional/homework/Application.java
+++ kodilla-stream/src/main/java/com/kodilla/optional/homework/Application.java
@@ -2,20 +2,30 @@ package com.kodilla.optional.homework;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class Application {
     public static void main(String[] args) {
-        List<Student> students = new ArrayList<>();
-        students.add(new Student("Anna Kowalska", new Teacher("Robert Januszek")));
-        students.add(new Student("Jan Wieczorek", new Teacher(null)));
-        students.add(new Student("Roman Romański", new Teacher("Krzysztof Cezary")));
-        students.add(new Student("Weronika Podsiadło", new Teacher(null)));
+            Teacher teacher1 = new Teacher("Adam Kowalski");
+            Teacher teacher2 = new Teacher("Bożena Nowak");
+            Teacher teacher3 = new Teacher("Jan Borysiewicz");
 
+            List<Student> students = new ArrayList<>();
+            students.add(new Student ("Żytomir Kowalski", teacher1));
+            students.add(new Student ("Anna Spaśna", null));
+            students.add(new Student ("Weronika Kulczyk", teacher2));
+            students.add(new Student ("Michał Workowy", teacher3));
+            students.add(new Student ("Lucjan Nowak", null));
 
-        for (Student student : students) {          // czy w tej pętli mogę użyć strumienia? np
+            Optional<Teacher> optionalTeacher = Optional.ofNullable(teacher1);
+            String teachername = optionalTeacher.orElse(new Teacher("undefinied")).getName();
 
+            for(Student student : students) {
+                System.out.println("uczeń: " + student.getName() + ", nauczyciel:" + student.getTeacher());
             }
 
+            }
+                                                                                            // coś tu poszło nie tak
 
         }
-   }
+
--- kodilla-stream/src/test/java/com/kodilla/stream/ForumStatsTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/ForumStatsTestSuite.java
@@ -1,4 +1,18 @@
 package com.kodilla.stream;
 
+import org.junit.jupiter.api.Test;
+
 public class ForumStatsTestSuite {
+    @Test
+    public void testAverageOfPostsOver40(){
+
+    }
+    @Test
+    public void testAverageOfPostsUnder40(){
+
+    }
+    @Test
+    public void shouldShowPeople(){
+
+    }
 }

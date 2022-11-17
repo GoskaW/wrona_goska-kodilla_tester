01a37d63c90b2336e4cc917b75557081c708c4f5
Malgorzata Wrona <wrona.goska@gmail.com>
2022-11-17 19:18:38 +0100

    App

--- kodilla-stream/src/test/java/com/kodilla/optional/homework/ApplicationTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/optional/homework/ApplicationTestSuite.java
@@ -2,15 +2,23 @@ package com.kodilla.optional.homework;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class ApplicationTestSuite {
 
     @Test
-    public void shouldShowCorrectTeacherName(){
+    public void testGetTeacherName() {
+        Teacher teacher = new Teacher("Michał Żytkowski");
+        Student student2 = new Student("Janina Bąk", teacher);
 
+        Application.getTeacherName(student2);
+        assertEquals(teacher, Application.getTeacherName(student2));
     }
 
     @Test
-    public void shouldReturnWhenNull(){
-
+    public void testNoTeacher () {
+        Student student = new Student ("Janina bąk", null);
+        Application.getTeacherName(student);
+        assertEquals("<undefined>", Application.getTeacherName(student));
     }
 }

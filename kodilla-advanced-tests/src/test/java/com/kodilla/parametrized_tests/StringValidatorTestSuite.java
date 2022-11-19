3f80a7e8bcee2a9a27f0c5fed4b5296b415f782d
Malgorzata Wrona <wrona.goska@gmail.com>
2022-11-19 14:39:00 +0100

    StringValidator

---
+++ kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/StringValidatorTestSuite.java
@@ -0,0 +1,25 @@
+package com.kodilla.parametrized_tests;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+public class StringValidatorTestSuite {
+
+    private StringValidator validator = new StringValidator();
+
+    @ParameterizedTest
+    @ValueSource(strings = {"", "   "})
+    public void shouldReturnTrueIfStringIsEmpty(String text) {
+        assertTrue(validator.isBlank(text));
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    public void shouldReturnTrueIfStringIsEmpty2(String text) {
+        assertTrue(validator.isBlank(text));
+    }
+}

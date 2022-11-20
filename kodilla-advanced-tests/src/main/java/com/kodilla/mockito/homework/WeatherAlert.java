3212672d0c75d77c104bd20813e9f0928b2ab7c0
Malgorzata Wrona <wrona.goska@gmail.com>
2022-11-20 16:44:34 +0100

    WeatherAlert

---
+++ kodilla-advanced-tests/src/main/java/com/kodilla/mockito/homework/WeatherAlert.java
@@ -0,0 +1,11 @@
+package com.kodilla.mockito.homework;
+
+import java.util.HashMap;
+import java.util.Set;
+
+public class WeatherAlert {
+
+   private HashMap<User, Set<Location>> userLocation = new HashMap<User, Set<Location>>();
+
+
+}

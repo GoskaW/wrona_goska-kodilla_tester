bbe7d2bcc1792dec185b075914b5143fcf2364eb
Malgorzata Wrona <wrona.goska@gmail.com>
2022-11-27 13:13:51 +0100

    gb

--- kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/GamblingMachineTestSuite.java
+++ kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/GamblingMachineTestSuite.java
@@ -14,7 +14,7 @@ public class GamblingMachineTestSuite {
 
     @ParameterizedTest
     @CsvFileSource(resources = "/correctNumbers.csv", numLinesToSkip = 1)
-    public void shouldReturnIfNumbersAreCorrect(int a, int b, int c, int d, int e, int f) throws InvalidNumbersException{
+    public void shouldReturnIfNumbersAreCorrect(int a, int b, int c, int d, int e, int f) throws InvalidNumbersException {
         Set<Integer> correctNumbers = new HashSet<>();
         correctNumbers.add(a);
         correctNumbers.add(b);
@@ -22,6 +22,19 @@ public class GamblingMachineTestSuite {
         correctNumbers.add(d);
         correctNumbers.add(e);
         correctNumbers.add(f);
-        assertThrows(InvalidNumbersException.class, ()->gamblingMachine.howManyWins(correctNumbers));
+        assertThrows(InvalidNumbersException.class, () -> gamblingMachine.howManyWins(correctNumbers));
+    }
+
+    @ParameterizedTest
+    @CsvFileSource (resources = "/incorrectNumbers.csv", numLinesToSkip = 1)
+    public void shouldReturnIfNumbersAreInncorrect (int a, int b, int c, int d, int e, int f) throws InvalidNumbersException {
+        Set<Integer> incorrectNumbers = new HashSet<>();
+        incorrectNumbers.add(a);
+        incorrectNumbers.add(b);
+        incorrectNumbers.add(c);
+        incorrectNumbers.add(d);
+        incorrectNumbers.add(e);
+        incorrectNumbers.add(f);
+        assertThrows(InvalidNumbersException.class, () -> gamblingMachine.howManyWins(incorrectNumbers));
     }
 }

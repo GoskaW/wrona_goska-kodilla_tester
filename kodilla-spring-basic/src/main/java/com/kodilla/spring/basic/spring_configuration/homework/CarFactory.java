fa91fcee5370f0d26fd829f280ccd36f5d7a7563
Malgorzata Wrona <wrona.goska@gmail.com>
2022-12-05 09:40:32 +0100

    9,4

--- kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/spring_configuration/homework/CarFactory.java
+++ kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/spring_configuration/homework/CarFactory.java
@@ -2,21 +2,25 @@ package com.kodilla.spring.basic.spring_configuration.homework;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Scope;
 
+import java.time.LocalDate;
 import java.time.LocalTime;
+import java.time.Month;
 
 @Configuration
+@Primary
 public class CarFactory {
 
     @Bean
     @Scope("prototype")
-    public Car choseCar(String season, LocalTime time) {
-        boolean turnOn = time.isAfter(LocalTime.of(20,00, 00));
-        boolean turnOff = time.isBefore(LocalTime.of(06, 00, 00));
-        if(season.equals("summer")) {
+    public Car choseCar() {
+        boolean turnOn = LocalTime.now().isAfter(LocalTime.of(20,00, 00) )&& LocalTime.now().isBefore(LocalTime.of(06, 00, 00));
+        LocalDate time = LocalDate.now();
+        if(time.getMonth() == Month.APRIL || time.getMonth() == Month.JUNE) {
             return new Cabrio();
-        } else if (season.equals("spring") || season.equals("autum")) {
+        } else if (time.getMonth() == Month.AUGUST|| time.getMonth() == Month.DECEMBER) {
             return new Sedan();
         }
             else {

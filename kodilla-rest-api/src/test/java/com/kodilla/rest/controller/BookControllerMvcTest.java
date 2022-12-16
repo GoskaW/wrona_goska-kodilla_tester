d2df45d9d2005f6e6cf23a111a6c802600f375b7
Malgorzata Wrona <wrona.goska@gmail.com>
2022-12-16 19:53:27 +0100

    11.3

---
+++ kodilla-rest-api/src/test/java/com/kodilla/rest/controller/BookControllerMvcTest.java
@@ -0,0 +1,35 @@
+package com.kodilla.rest.controller;
+
+import com.kodilla.rest.domain.BookDto;
+import com.kodilla.rest.service.BookService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@ExtendWith(MockitoExtension.class)
+@WebMvcTest(BookController.class)                   // [1]
+public class BookControllerMvcTest {
+
+    @Autowired
+    private MockMvc mockMvc;                         // [2]
+
+    @MockBean
+    private BookService bookService;                 // [3]
+
+    @Test
+    public void shouldFetchBooks() throws Exception {
+        //given
+        List<BookDto> booksList = new ArrayList<>();
+        booksList.add(new BookDto("title 1", "author 1"));
+        booksList.add(new BookDto("title 2", "author 2"));
+        Mockito.when(bookService.getBooks()).thenReturn(booksList);
+    }
+}

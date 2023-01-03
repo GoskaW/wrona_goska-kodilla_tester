6c9053904839449a172d10db7b4d5e1c9a067e19
Malgorzata Wrona <wrona.goska@gmail.com>
2023-01-03 17:59:36 +0100

    13.3

--- kodilla-selenium/src/main/java/com/kodilla/selenium/ebay/EBayTestingApp.java
+++ kodilla-selenium/src/main/java/com/kodilla/selenium/ebay/EBayTestingApp.java
@@ -8,11 +8,11 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class EBayTestingApp {
 
     public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver", "C:\\selenium-drivers\\Chrome\\chromedriver_win32\\chromedriver.exe");  	// [1]
+        System.setProperty("webdriver.chrome.driver", "C:\\selenium-drivers\\Chrome\\chromedriver_win32\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.ebay.com/");
 
-        WebElement inputField = driver.findElement(By.name("_nkw")); // [4]
+        WebElement inputField = driver.findElement(By.name("_nkw"));
         inputField.sendKeys("Laptop");
         inputField.submit();
     }
